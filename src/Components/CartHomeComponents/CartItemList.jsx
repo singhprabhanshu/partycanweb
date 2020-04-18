6c549e4051966880fabab4225738d964255f31b9
vishnu <vkmyself1984@gmail.com>
2020-04-19 00:07:54 +0530

    ui changes

--- src/Components/CartHomeComponents/CartItemList.jsx
+++ src/Components/CartHomeComponents/CartItemList.jsx
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import _ from "lodash";
 import {Container, Row, Col} from 'reactstrap'
 import proImg from '../../assets/images/party-can.png';
+import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
+
 
 
 class CartItemList extends React.Component {
@@ -77,39 +79,29 @@ class CartItemList extends React.Component {
             return (
                 <React.Fragment>
                 <Container fluid={true}>                
-                    <Row className="no-gutters justify-content-lg-between secMinHeight">
-                    <Col lg={5} className="order-1 d-none d-lg-block order-md-2">
-                        <div className="productImgSection">
-                                <img src={proImg} className="imgProduct img-responsive"></img>
-                         </div>
-                        </Col>
-                        <Col lg={6} className="p-5  d-flex order-2 order-md-1">
-                <div key={key} className="CarItemMain">
-                    <div className="cartItemChild">
-                        <div className="d-flex justify-content-lg-between align-items-center ">
-                        <img height={50} width={50} src={item.image} alt="Product Image" />
-                        <div>{item.name}</div>
-                        </div>
-                        <div>
-                            <i onClick={()=>this.updateQty(item,-1,key)}  style={{cursor:"pointer"}} class="fa fa-minus-circle cart-minus-icon" aria-hidden="true"></i>
-                             <span>{`   ${item.qty}   `}</span>
-                              <i onClick={()=>this.updateQty(item,+1,key)} style={{cursor:"pointer"}} class="fa fa-plus-circle" aria-hidden="true"></i>
-                        </div>
-                        {this.props.cartIsFetching?<span>Loading</span>:<span>{item.row_total}</span>}
-                    </div>
-                    <div className="remove-cart-icon" onClick={() => this.handleCartRemoveItem(item)}>
-                        <span>
-                            <i className="fa fa-times" aria-hidden="true"></i>
-                        </span>
-                        <div>
-                            Remove
-                    </div>
-                    </div>
-                </div>
+                    <Row className="no-gutters">
+                        <Col className="d-flex order-2 order-md-1 align-self-start">
+                            <div key={key} className="CarItemMain align-items-center justify-content-between no-gutters row" >
+                                <div className="col-11 cartItemChild">
+                                    <div className="d-flex justify-content-lg-between align-items-center ">
+                                    <img height={50} width={50} src={item.image} alt="Product Image" />
+                                    <div>{item.name}</div>
+                                    </div>
+                                    <div>
+                                        <i onClick={()=>this.updateQty(item,-1,key)}  style={{cursor:"pointer"}} class="fa fa-minus-circle cart-minus-icon" aria-hidden="true"></i>
+                                        <span>{`   ${item.qty}   `}</span>
+                                        <i onClick={()=>this.updateQty(item,+1,key)} style={{cursor:"pointer"}} class="fa fa-plus-circle" aria-hidden="true"></i>
+                                    </div>
+                                    {this.props.cartIsFetching?<span>Loading</span>:<span>{item.row_total}</span>}
+                                </div>
+                                <div className="col-auto ml-3 remove-cart-icon" onClick={() => this.handleCartRemoveItem(item)}>
+                                    <div className="mb-2"><CloseOutlinedIcon style={{ fontSize: 25 }} /> </div> 
+                                    <div>Remove</div>
+                                </div>
+                            </div>
                  </Col>
                 </Row>
-                </Container>
-               
+                </Container>               
             </React.Fragment>
             )
         })
--- src/Containers/Cart/CartContainer.jsx
+++ src/Containers/Cart/CartContainer.jsx
@@ -50,14 +50,14 @@ class CartContainer extends React.Component {
 
     renderContent = (cardWidth,coupon_code,itemRemovedFetching,itemUpdatedFetching,cartIsFetching,discount,subTotal,grandTotal,feeAmount  ) => {
         let commonContent = <>
-        <div className="cartContainer">
+        <div className="cartContainer pr-lg-4">
                 <div className="CartItemParent">
                     <CartItemsList
                         dispatch={this.props.dispatch}
                         width={cardWidth}
                         cartItems={this.props.cartItems} />
                 </div>
-                <div className="couponParent">
+                <div className="couponParent mt-5">
                     <CouponCode onChange={this.onChange} width={cardWidth} coupon_code={coupon_code} />
                 </div>
                 <div style={{ width: cardWidth }} className="PriceSummaryParent">
@@ -76,7 +76,7 @@ class CartContainer extends React.Component {
             return <div>{commonContent}</div>
         }
         else{
-        return <Scrollbar  className="leftSecmaxHeight">{commonContent}</Scrollbar>
+        return <Scrollbar className="leftSecmaxHeight">{commonContent}</Scrollbar>
         }
       }
 
