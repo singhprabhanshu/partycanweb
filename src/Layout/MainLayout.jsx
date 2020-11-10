a60aa850e36686ab62487f1a760192575b808e3d
Aman Khandelwal <amankhandelwal.cs@gmail.com>
2020-11-10 09:42:31 +0530

    #comments godaddy security seal integration

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -11,7 +11,7 @@ import Footer from '../Global/UIComponents/Footer';
 
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import {isMobile, isTablet} from 'react-device-detect';
+import { isMobile, isTablet } from 'react-device-detect';
 import Scrollbar from "react-scrollbars-custom";
 const stripePromise = loadStripe("pk_test_RkUSbyxxcn4xFQ0ttE6QsIDh00bGPMtJdc");
 //const stripePromise = loadStripe("pk_live_YWHAuQZ9QVoqDq3bOdRRz8Rg006Mq0WrB9");
@@ -32,22 +32,22 @@ class MainLayout extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        showUserMenuOption: false
+      showUserMenuOption: false
     }
-}
+  }
 
   showUserMenu = () => {
     this.setState({
-        showUserMenuOption: !this.state.showUserMenuOption
+      showUserMenuOption: !this.state.showUserMenuOption
     });
   }
-  
+
   hideUserMenu = () => {
-    if(this.state.showUserMenuOption === true) {
+    if (this.state.showUserMenuOption === true) {
       this.setState({
         showUserMenuOption: false
-      }); 
-    }   
+      });
+    }
   }
 
   handleRoute() {
@@ -55,56 +55,56 @@ class MainLayout extends React.Component {
     switch (currentRoute) {
       case "/splash":
         return <Footer isLoginAndSignupScreen={true} {...this.props} showUserMenu={this.showUserMenu}
-        hideUserMenu={this.hideUserMenu} howUserMenuOption={this.state.showUserMenuOption} />;
+          hideUserMenu={this.hideUserMenu} howUserMenuOption={this.state.showUserMenuOption} />;
       case "/cart/:cartflow":
         return <Footer isLoginAndSignupScreen={true} {...this.props} showUserMenu={this.showUserMenu}
-        hideUserMenu={this.hideUserMenu}
-        showUserMenuOption={this.state.showUserMenuOption}/>;
+          hideUserMenu={this.hideUserMenu}
+          showUserMenuOption={this.state.showUserMenuOption} />;
       default:
         return <Footer isLoginAndSignupScreen={false} {...this.props} showUserMenu={this.showUserMenu}
-        hideUserMenu={this.hideUserMenu}
-        showUserMenuOption={this.state.showUserMenuOption} />;
+          hideUserMenu={this.hideUserMenu}
+          showUserMenuOption={this.state.showUserMenuOption} />;
     }
   }
 
   renderContent = (classes) => {
-    let commonContent =  <>
-    <div  onClick={this.hideUserMenu}>
-      <HeaderBar history={this.props.history} showUserMenu={this.showUserMenu}
-       showUserMenuOption={this.state.showUserMenuOption}/></div>
-    <div className="container-content-section" onClick={this.hideUserMenu}>
-    <Elements stripe={stripePromise}>{this.props.children}</Elements>
-     <div>{this.props.message.text && <Snackbar
-       anchorOrigin={{
-         vertical: 'bottom',
-         horizontal: 'right',
-       }}
-       open={true}
-       autoHideDuration={6000}
-       onClose={() => { }}
-       ContentProps={{
-         'aria-describedby': 'message-id',
-         classes: {
-           root: this.props.message.isSuccess ? classes.success : classes.failure
-         }
-       }}
-       message={<span id="message-id">{this.props.message.text}</span>}
-     />}
-     </div>
-     </div>
-     {
-     this.handleRoute()
+    let commonContent = <>
+      <div onClick={this.hideUserMenu}>
+        <HeaderBar history={this.props.history} showUserMenu={this.showUserMenu}
+          showUserMenuOption={this.state.showUserMenuOption} /></div>
+      <div className="container-content-section" onClick={this.hideUserMenu}>
+        <Elements stripe={stripePromise}>{this.props.children}</Elements>
+        <div>{this.props.message.text && <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          open={true}
+          autoHideDuration={6000}
+          onClose={() => { }}
+          ContentProps={{
+            'aria-describedby': 'message-id',
+            classes: {
+              root: this.props.message.isSuccess ? classes.success : classes.failure
+            }
+          }}
+          message={<span id="message-id">{this.props.message.text}</span>}
+        />}
+        </div>
+      </div>
+      {
+        this.handleRoute()
+      }
+
+    </>
+    if (isMobile || isTablet) {
+      return <div className="mainLayout">{commonContent}</div>
+    }
+    else {
+      return <Scrollbar className="mainLayout">{commonContent}</Scrollbar>
+    }
   }
-     
-   </>
-   if(isMobile || isTablet){
-   return <div className="mainLayout">{commonContent}</div>
-   }
-   else{
-   return <Scrollbar className="mainLayout">{commonContent}</Scrollbar>
-   }
-  }
-  
+
   componentDidMount() {
     window.scrollTo(0, 0);
   }
@@ -112,7 +112,7 @@ class MainLayout extends React.Component {
     let { classes } = this.props;
     return (
       <>
-      {this.renderContent(classes)}
+        {this.renderContent(classes)}
       </>
     );
   }
